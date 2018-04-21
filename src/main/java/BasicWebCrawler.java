953b175b892274558427a08e5855cc0b85a36623
aditya-sanghi <aditya.sng@gmail.com>
2018-04-21 17:20:13 +0800

    adds basic logic

--- src/main/java/BasicWebCrawler.java
+++ src/main/java/BasicWebCrawler.java
@@ -1,2 +1,38 @@
+package com.mkyong.basicwebcrawler;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.HashSet;
+
 public class BasicWebCrawler {
-}
+
+  private HashSet<String> links;
+
+  public BasicWebCrawler() {
+    links = new HashSet<String>();
+  }
+
+  public void getPageLinks(String URL) {
+
+    try {
+      Document document = Jsoup.connect(URL).get();
+      System.out.println(document);
+
+      Elements linksOnPage = document.select("a[href]");
+
+      for(Element link : linksOnPage) {
+        System.out.println(link);    
+      }
+    } catch (IOException e) {
+      System.err.println("For '" + URL + "': " + e.getMessage());
+    }
+}    
+
+    public static void main(String[] args) {
+      new BasicWebCrawler().getPageLinks("http://www.mkyong.com/");
+    }
+  }
